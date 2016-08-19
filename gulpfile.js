00a18b7c9cf71df4ea288dd16d1c59dc5cd3bc62
analyst165 <omcrobbie@oem.nyc.gov>
2016-08-18 21:54:42 -0400

    'testing'

--- gulpfile.js
+++ gulpfile.js
@@ -3,26 +3,57 @@ const browserSync = require('browser-sync'),
     babel = require('gulp-babel'),
     concat = require('gulp-concat'),
     ngAnnotate = require('gulp-ng-annotate'),
-    uglify = require('gulp-uglify');
+    uglify = require('gulp-uglify'),
+    runSeq = require('run-sequence'),
+    sass = require('gulp-sass'),
+    plumber = require('gulp-plumber'),
+    rename = require('gulp-rename'),
+    notify = require('gulp-notify');
 
 gulp.task('serve',['build'], function(){
     browserSync({
         server: {
             baseDir: ['site', 'bower_components', 'library']
         },
+        browser: 'chrome',
         open: true,
         port: 3000,
         notify: false
     })
-    gulp.watch(['./site/**/*.html', './site/**/*.js'], ['build', browserSync.reload ]);
+    gulp.watch(['./site/app/**/*.html', './site/app/**/*.js', './site/scss/**/*.scss'], ['build', browserSync.reload ]);
 });
 
-gulp.task('build', [], function(){
-    gulp.src(['./site/app.js', './site/common/**/*.js'])
+gulp.task('build',function(){
+    runSeq(['compileJS', 'buildCSS']);
+});
+
+gulp.task('compileJS', [], function(){
+    gulp.src(['./site/app.js', './site/app/**/*.js', './site/app/**/**/*.js'])
     .pipe(concat('main.js'))
     .pipe(babel())
     .pipe(ngAnnotate())
     .pipe(gulp.dest('./site'));
-})
+});
 
-gulp.task('default', ['serve']);
+gulp.task('compileCSS', function() {
+    return gulp.src('./site/scss/**/*.css')
+        .pipe(concat('style.css'))
+        .pipe(gulp.dest('./css'))
+});
+
+gulp.task('buildCSS', function () {
+    var sassCompilation = sass();
+    sassCompilation.on('error', console.error.bind(console));
+
+    return gulp.src('./site/scss/main.scss')
+        .pipe(plumber({
+            errorHandler: notify.onError('SASS processing failed! Check your gulp process.')
+        }))
+        .pipe(sassCompilation)
+        .pipe(rename('style.css'))
+        .pipe(gulp.dest('./site/css'))
+});
+
+gulp.task('default', function(){
+    gulp.start('serve');
+});
--- site/app.js
+++ site/app.js
@@ -1,1 +1,16 @@
-var app = angular.module('app', ['esri.map']);
+var app = angular.module('app', []);
+
+app.controller('MapCtrl', function ($scope) {
+    var map;
+    require([
+        'esri/map'
+    ], function(Map){
+        map = new Map("map", {
+            basemap: "streets",
+            center: [-119.036, 36.621],
+            zoom: 6,
+            logo: false,
+            showAttribution: false
+        });
+    })
+});
